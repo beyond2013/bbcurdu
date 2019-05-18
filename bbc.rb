f40649b69f06b1703e961c6a2f6bf936ecd4d722
Imran Ali <imran.cs.uob@gmail.com>
2019-05-18 14:04:02 +0500

    restructuring scrapper, add author and tag list to contents

--- bbc.rb
+++ bbc.rb
@@ -1,27 +1,44 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-print "Enter url of the bbc news article: \t"
-url = gets
+
+def scrap(url)
 encoded_url = URI.encode(url)
 doc = Nokogiri::HTML(open(URI.parse(encoded_url)))
-@notice =""
-#	node = doc.search("//div[@class='g-container']/h1")
+@article =""
 	node = doc.search("//div[@class='story-body']/h1")
-	@notice +="#{node.text}\n"
-	#nodes = doc.search("//div[@class='bodytext']/p")
+	@article +="#{node.text}\n"
+	author = doc.search("//span[@class='byline__name']")
+	@article +="#{author.text}\n"
+	tags = doc.search("//ul[@class='tags-list']/li/a")
+	tags.each do |tag|
+		if tag.child && tag.child.text?
+			@article += "#{tag.child}\t"
+		end
+	end
+	@article += "\n"
 	nodes = doc.search("//div[@class='story-body__inner']/p")
 	nodes.each do |node|
 		if node.child && node.child.text?
-			@notice +=  "#{node.child}\n"
+			@article +=  "#{node.child}\n"
 		end
 	end
+	return @article
+end
 
+def writeToFile(contents)
 filename = "./data/"
 print "Enter file name to save the contents of news article: \t"
 filename << gets.chomp
 filename << ".txt"
 fh = File.new(filename.chomp, "w")
-fh.puts @notice
+fh.puts contents
 fh.close
 puts "contents written to file #{filename}" 
+end
+
+
+print "Enter url of the bbc news article: \t"
+url = gets
+contents= scrap(url)
+writeToFile(contents)
